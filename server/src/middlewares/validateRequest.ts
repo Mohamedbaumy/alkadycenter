abc489c1e0cb98f635ca4c944237d4a49a43c2b6
Mohamed Bayoumi <52928159+Mohamedbaumy@users.noreply.github.com>
2024-10-19 20:52:43 +0300

    feat:

--- server/src/middlewares/validateRequest.ts
+++ server/src/middlewares/validateRequest.ts
@@ -1,11 +1,12 @@
 import type { Request, Response, NextFunction } from "express";
 import { validationResult } from "express-validator";
+import { sendResponse } from "./errorHandler";
 
 const validateRequest = (req: Request, res: Response, next: NextFunction) => {
 	const errors = validationResult(req);
 	if (!errors.isEmpty()) {
 		console.log(errors);
-		return res.status(400).json({ errors: errors.array() });
+		return sendResponse(res, "Invalid request", errors.array());
 	}
 	next();
 };
